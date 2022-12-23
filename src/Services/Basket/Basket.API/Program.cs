8363060d600748b1ffb001e7832e1cf184b56a9a
Miroslav Hristov <mhris@softserveinc.com>
2022-12-23 17:08:59 +0200

    add and configure redis

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddStackExchangeRedisCache(opt => 
+{
+     opt.Configuration = builder.Configuration.GetValue<string>(
+        "DistributedCacheConfig:ConnectionString");
+});
 
 var app = builder.Build();
 
