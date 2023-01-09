6874db3a811077ada774a8f05a7d09d6a93ff592
Miroslav Hristov <mhris@softserveinc.com>
2023-01-09 15:27:50 +0200

    delete default grpc service

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -4,7 +4,6 @@ using Npgsql;
 using Discount.Grpc.Data;
 using Discount.Grpc.Repositories;
 using Discount.Grpc.Extensions;
-using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,7 +27,6 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
