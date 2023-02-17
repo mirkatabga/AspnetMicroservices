a42b95f0552cb9b939cdc46ffe57a4305873de69
Miroslav Hristov <mhris@softserveinc.com>
2023-02-17 16:31:52 +0200

    :page_with_curl: add validaiton error logging

--- src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -1,22 +1,25 @@
 using FluentValidation;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using ValidationException = Ordering.Application.Exceptions.ValidationException;
 
 namespace Ordering.Application.Behaviours
 {
-    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> 
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
         where TRequest : IRequest<TResponse>
     {
         private readonly IEnumerable<IValidator<TRequest>> _validators;
+        private readonly ILogger<TRequest> _logger;
 
-        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators, ILogger<TRequest> logger)
         {
             _validators = validators ?? throw new ArgumentNullException(nameof(validators));
+            _logger = logger;
         }
 
         public async Task<TResponse> Handle(
-            TRequest request, 
-            RequestHandlerDelegate<TResponse> next, 
+            TRequest request,
+            RequestHandlerDelegate<TResponse> next,
             CancellationToken cancellationToken)
         {
             if (_validators.Any())
@@ -25,6 +28,11 @@ namespace Ordering.Application.Behaviours
 
                 var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
                 var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+                foreach (var failure in failures)
+                {
+                    _logger.LogWarning("Validation error: {PropertyName} - Error: {ErrorMessage}", failure.PropertyName, failure.ErrorMessage);
+                }
 
                 if (failures.Count != 0)
                     throw new ValidationException(failures);
